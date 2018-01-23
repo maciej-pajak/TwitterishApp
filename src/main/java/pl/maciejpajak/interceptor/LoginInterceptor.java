b47cf2298f823c2f611b9b2bdb6e343df03cf504
maciej-pajak <maciek.pajak@vp.pl>
2018-01-24 00:00:43 +0100

    fix attr name

--- src/main/java/pl/maciejpajak/interceptor/LoginInterceptor.java
+++ src/main/java/pl/maciejpajak/interceptor/LoginInterceptor.java
@@ -6,12 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import pl.maciejpajak.util.Consts;
+
 public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        Long id = (Long) request.getSession().getAttribute("LOGGED_IN_USER_ID");
+        Long id = (Long) request.getSession().getAttribute(Consts.LOGGED_ID);
         if (id == null) {
             response.sendRedirect(request.getContextPath() + "/login");
             return false;
